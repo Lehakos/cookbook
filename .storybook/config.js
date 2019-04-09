b672e155cbd9dd91580b2fe634100f50897cde35
Aleksei Kosianenko <lehakos@gmail.com>
2019-04-09 09:26:18 +0300

    chore(storybook): изменен конфиг сторибука

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,23 +1,41 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components/macro';
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withConsole } from '@storybook/addon-console';
 import { create } from '@storybook/theming';
+import { withKnobs } from '@storybook/addon-knobs';
+import 'normalize.css';
+
+import GlobalStyles from 'globalStyles';
+import theme from 'shared/theme';
 
 addParameters({
   options: {
     theme: create({
       base: 'light',
-      brandTitle: 'CookBook ui-kit'
+      brandTitle: 'CookBook ui-kit',
     }),
     sortStoriesByKind: true,
     hierarchyRootSeparator: '|',
-    hierarchySeparator: '/'
-  }
+    hierarchySeparator: '/',
+  },
 });
 
 addDecorator(withInfo());
 
+addDecorator(withKnobs);
+
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <div style={{ padding: '12px' }}>
+      <GlobalStyles />
+      {story()}
+    </div>
+  </ThemeProvider>
+));
 
 const req = require.context('../src', true, /\.story\.tsx?$/);
 
