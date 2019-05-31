eb00ca6aec126acbcd6c6eb912013702fc5c2708
Aleksei Kosianenko <lehakos@gmail.com>
2019-05-31 14:00:39 +0300

    feat: добавлена директория с общими типами

---
+++ src/shared/types/StrictUnion.ts
@@ -0,0 +1,10 @@
+type UnionKeys < T > = T extends any ? keyof T : never;
+type StrictUnionHelper < T, TAllKeys extends PropertyKey > = T extends any
+  ? T & Partial<Record<Exclude<TAllKeys, keyof T>, never>>
+  : never;
+
+/**
+ * Тип для правильной работы Discriminated Union типов
+ * https://stackoverflow.com/questions/52677576/typescript-discriminated-union-allows-invalid-state/52678379#52678379
+ */
+export type StrictUnion < T > = StrictUnionHelper<T, UnionKeys<T>>;
