ebdb3ec1c34884e3b116bbf11cc99485adbdbb9e
Aleksei Kosianenko <lehakos@gmail.com>
2019-05-31 22:54:45 +0300

    feat(app): добавлен ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from 'libs/theme';
 
 import GlobalStyles from './GlobalStyles';
 
 const App: React.FunctionComponent<{}> = function () {
   return (
-    <div>
-      <GlobalStyles />
-      Hello
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <GlobalStyles />
+      </div>
+    </ThemeProvider>
   );
 };
 
