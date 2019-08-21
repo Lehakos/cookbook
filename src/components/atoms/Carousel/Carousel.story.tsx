bb7b77c58149d9eefb5bc04d9abe0e0feb346505
Aleksei Kosianenko <lehakos@gmail.com>
2019-08-21 18:53:46 +0300

    feat(Carousel): add component

---
+++ src/components/atoms/Carousel/Carousel.story.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+
+import { boolean, number } from '@storybook/addon-knobs';
+import Carousel from './Carousel';
+
+const stories = storiesOf('Atoms|Carousel', module);
+
+const SlideWrapper = styled.div`
+  height: 250px;
+
+  background: ${props => props.theme.colors.grays[0]};
+`;
+
+const Slide = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+`;
+
+stories.add('Default', () => (
+  <Carousel
+    arrows={boolean('arrows', true)}
+    autoplaySpeed={number('autoplaySpeed', 3000, {
+      step: 1000,
+      range: false,
+      min: 0,
+      max: 100000,
+    })}
+    autoplay={boolean('autoplay', false)}
+    centerMode={boolean('centerMode', false)}
+    dots={boolean('dots', false)}
+    slidesToShow={number('slidesToShow', 1)}
+  >
+    <SlideWrapper>
+      <Slide>1</Slide>
+    </SlideWrapper>
+    <SlideWrapper>
+      <Slide>2</Slide>
+    </SlideWrapper>
+    <SlideWrapper>
+      <Slide>3</Slide>
+    </SlideWrapper>
+  </Carousel>
+));
---
+++ src/components/atoms/Carousel/Carousel.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Slick from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+
+import theme from 'shared/theme';
+
+import * as s from './Carousel.styles';
+
+interface Props {
+  arrows?: boolean;
+  autoplaySpeed?: number;
+  autoplay?: boolean;
+  centerMode?: boolean;
+  centerPadding?: string;
+  className?: string;
+  dots?: boolean;
+  infinite?: boolean;
+  lazyLoad?: 'ondemand' | 'progressive';
+  slidesToShow?: number;
+}
+
+const Carousel: React.FunctionComponent<Props> = function({
+  children,
+  arrows = true,
+  autoplaySpeed = 3000,
+  autoplay = false,
+  centerMode = false,
+  centerPadding = `${theme.space[6]}px`,
+  className,
+  dots = false,
+  infinite = true,
+  lazyLoad,
+  slidesToShow = 1,
+}) {
+  return (
+    <Slick
+      nextArrow={<s.ArrowButton />}
+      prevArrow={<s.ArrowButton prev />}
+      arrows={arrows}
+      autoplaySpeed={autoplaySpeed}
+      autoplay={autoplay}
+      centerMode={centerMode}
+      centerPadding={centerPadding}
+      className={className}
+      dots={dots}
+      infinite={infinite}
+      lazyLoad={lazyLoad}
+      slidesToShow={slidesToShow}
+    >
+      {children}
+    </Slick>
+  );
+};
+
+export default Carousel;
